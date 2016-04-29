8bbd85e24e8872b521a9c6e53dcd4e890dc745c4
Jack Hanford <jackhanford@gmail.com>
2016-04-28 18:39:56 -0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = require('angular')
+  .module('ngMaxLength', [])
+  .directive('ngMaxlength', ngMaxLength)
+  .name
+
+function ngMaxLength () {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attrs, ngModelCtrl) {
+      var maxlength = Number(attrs.ngMaxlength)
+      function fromUser(data) {
+        if (data.length > maxlength) {
+          var transformedInput = data.substring(0, maxlength)
+          ngModelCtrl.$setViewValue(transformedInput)
+          ngModelCtrl.$render()
+          return transformedInput
+        }
+        return data
+      }
+      ngModelCtrl.$parsers.push(fromUser)
+    }
+  }
+}
